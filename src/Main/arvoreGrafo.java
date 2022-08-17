e50aab1b7d25f701fda0e3caf9115f5ac0644442
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-08-17 11:59:19 -0300

    iniciei preenchimento da matriz

--- src/Main/arvoreGrafo.java
+++ src/Main/arvoreGrafo.java
@@ -1,17 +1,51 @@
 package Main;
 
 public class arvoreGrafo {
-	private char x, o;
+	private char Symbol = 'o';
 	private int max = 9;
-	private char[][] no = new char[3][3];
+	private char[][] no = 	{{'0','0','0'},
+							{'0','0','0'},
+							{'0','0','0'}}
+	;
 	private TNojo noAtual;
 	
 	public void Gerar() {
+		while(true) { //rodar enquanto houver estados pra gerar
+			for(int i = 0; i < 3; i++) { //percorrer a matriz de caracteres
+				for(int j = 0; j < 3; j++) {
+					if(no[i][j] != 'x' && no[i][j] != 'o') {
+						no[i][j] = Symbol;
+						changeSymbol();
+					}
+				}
+			}
+			break;
+		}
+	}
 	
+	public void mostrar() {
+		
+		System.out.println("-------");
+		
+		for(int i = 0; i < 3; i++) { //percorrer a matriz de caracteres
+			for(int j = 0; j < 3; j++) {
+				System.out.print(no[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 	private boolean verificaGanhador() {
 		
 		return true;
 	}
+	
+	private void changeSymbol() {
+		if(Symbol == 'x') {
+			Symbol = 'o';
+		}else {
+			Symbol = 'x';
+		}
+	}
+	
 }
--- src/Main/main.java
+++ src/Main/main.java
@@ -3,7 +3,11 @@ package Main;
 public class main {
 
 	public static void main(String[] args) {
+		arvoreGrafo grafo = new arvoreGrafo();
 		
+		grafo.mostrar();
+		grafo.Gerar();
+		grafo.mostrar();
 
 	}
 
